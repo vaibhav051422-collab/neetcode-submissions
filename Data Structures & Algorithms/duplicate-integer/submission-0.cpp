8946ce6a89ee1bb89c89ee30d7e68f4ed590e540
Vaibhav Pandey <vaibhav051422@gmail.com>
2026-09-24 18:24:07 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(int x:nums){
+            mp[x]++;
+        }
+        for(auto it :mp){
+            if(it.second>1){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+};
